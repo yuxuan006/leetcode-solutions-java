6158dbc872a8e44c492a534f618a143e2587bc46
yuchai006 <xuan@Yuxuan-Chais-MacBook-Air.local>
2015-08-27 22:42:03 -0400

    Number of Islands

---
+++ numOfIslands.java
@@ -0,0 +1,51 @@
+//Number of Islands
+/*
+ Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+ 
+ Example 1:
+ 
+ 11110
+ 11010
+ 11000
+ 00000
+ Answer: 1
+ 
+ Example 2:
+ 
+ 11000
+ 11000
+ 00100
+ 00011
+ Answer: 3
+*/
+public class Solution {
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        int row = grid.length;
+        if(row == 0)return 0;
+        int col = grid[0].length;
+        if(col == 0)return 0;
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(grid[i][j] != '1')continue;
+                
+                count++;
+                dfs(grid, i, j);
+            }
+        }
+        return count;
+    }
+    public void dfs(char[][] grid, int i, int j){
+        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length){
+            return;
+        }
+        if(grid[i][j] == '1'){
+            grid[i][j] = '2';
+            dfs(grid, i-1, j);
+            dfs(grid, i+1, j);
+            dfs(grid, i, j-1);
+            dfs(grid, i, j+1);
+        }
+    }
+    
+}
