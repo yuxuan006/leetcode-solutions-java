1996d04699e101dabea4cae2e55c236601de9210
yuchai006 <xuan@Yuxuan-Chais-MacBook-Air.local>
2015-08-27 21:15:13 -0400

    unique path2

--- uniquePath2.java
+++ uniquePath2.java
@@ -21,24 +21,34 @@ public class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         if(obstacleGrid[0][0] == 1 || obstacleGrid.length == 0 || obstacleGrid[0].length == 0)return 0;
         int row = obstacleGrid.length, col = obstacleGrid[0].length;
-        int[][] res = new int[row+2][col+2];
+        int[][] res = new int[row][col];
         for(int i = 0; i < row; i++){
             for(int j = 0; j < col; j++){
                 res[i][j] = 0;
             }
         }
-        res[row][col+1] = 1;
-        for(int i = row; i >= 1; i--){
-            for(int j = col; j >= 1; j--){
-                if(obstacleGrid[i-1][j-1] == 1){
-                    res[i][j] = 0;
-                }
-                else{
-                    res[i][j] = res[i+1][j]+ res[i][j+1];
+        if(obstacleGrid[0][0] == 1)return 0;
+        else
+            res[0][0] = 1;
+        for(int i = 1; i < row; i++){
+            if(obstacleGrid[i][0] == 0){
+                res[i][0] = res[i-1][0];
+            }
+        }
+        for(int i = 1; i < col; i++){
+            if(obstacleGrid[0][i] == 0){
+                res[0][i] = res[0][i-1];
+            }
+        }
+        for(int i = 1; i < row; i++){
+            for(int j = 1; j < col; j++){
+                if(obstacleGrid[i][j] == 0){
+                    res[i][j] = res[i-1][j] + res[i][j-1];
                 }
             }
         }
-        return res[1][1];
+        
+        return res[row-1][col-1];
     }
     
 }
