5ecbb910002993d87ff5b1388d747897a5c45e69
yuchai006 <xuan@Yuxuan-Chais-MacBook-Air.local>
2015-09-30 17:13:44 -0400

    update iterative solution

--- BinaryTreePostorderTraversal.java
+++ BinaryTreePostorderTraversal.java
@@ -38,3 +38,54 @@ public class Solution {
     }
     
 }
+//iterative using two stacksßß
+public class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList();
+        if(root == null)return res;
+        TreeNode cur = root;
+        Stack<TreeNode> s1 = new Stack<TreeNode>();
+        s1.push(root);
+        Stack<TreeNode> s2 = new Stack<TreeNode>();
+        while(s1.empty() == false){
+            TreeNode temp = s1.pop();
+            s2.push(temp);
+            if(temp.left!=null)s1.push(temp.left);
+            if(temp.right!=null)s1.push(temp.right);
+        }
+        while(s2.empty() == false){
+            res.add(s2.pop().val);
+        }
+        return res;
+    }
+}
+//iterative using one stack
+public class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList();
+        if(root == null)return res;
+        TreeNode cur = root;
+        Stack<TreeNode> s = new Stack<TreeNode>();
+        do{
+            while(root != null){
+                if(root.right != null){
+                    s.push(root.right);
+                }
+                s.push(root);
+                root = root.left;
+            }
+            
+            root = s.pop();
+            if(s.empty() == false&&root.right!=null && s.peek() == root.right){
+                s.pop();
+                s.push(root);
+                root = root.right;
+            }else{
+                res.add(root.val);
+                root = null;
+            }
+            
+        }while(s.empty() == false);
+        return res;
+    }
+}
