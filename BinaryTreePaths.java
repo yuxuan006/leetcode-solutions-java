2513ae0a7aeb704ff0a6164ad4832b8a2cdbf64a
yuchai006 <xuan@Yuxuan-Chais-MacBook-Air.local>
2015-09-30 16:07:01 -0400

    Binary Tree Paths

--- BinaryTreePaths.java
+++ BinaryTreePaths.java
@@ -1,6 +1,4 @@
 //Binary Tree Paths
-/*
- Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.*/
 /**
  Given a binary tree, return all root-to-leaf paths.
  
